26b5052e4352d8dc6a0ba4873424cf40880d4f38
harrypotter0 <9654263057akashkandpal@gmail.com>
2017-11-03 08:11:18 +0530

    Server is under experimentation

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+  MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
+  MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule } from '@angular/material';
+  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+  import { MaterialModule } from '@angular/material';
+  import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+   MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+   MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
+   MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule,    FlexLayoutModule,
+    BrowserModule,  BrowserAnimationsModule,MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
